4b6ec9b52042f106b5385a6fe6175598e568cbfb
oxb <oxb@zhijian.run>
2021-05-29 09:14:19 +0800

    change struct

--- src/scifanchain/settings/develop.py
+++ src/scifanchain/settings/develop.py
@@ -4,6 +4,6 @@ from ._base import *
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'sfifan_db.sqlite3',
+        'NAME': BASE_DIR / 'scifanchain_db.sqlite3',
     }
 }
