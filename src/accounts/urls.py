593ead79e03b052044a13c4e7513a607b64f66a7
oxb <oxb@zhijian.run>
2021-05-31 11:35:05 +0800

    update something

--- src/accounts/urls.py
+++ src/accounts/urls.py
@@ -6,8 +6,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('register/', views.register, name='register'),
+    path('signup/', views.signup, name='signup'),
     path('login/', auth_views.LoginView.as_view(), name='login'),
-    path('logout/', views.user_logout, name='logout'),
     path('dashboard/', views.dashboard, name='dashboard'),
 ]
--- src/scifanchain/urls.py
+++ src/scifanchain/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
     path('accounts/', include('accounts.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
     # path('blogs/', include('blogs.urls')),
     # path('eras/', include('eras.urls')),
     # path('persons/', include('persons.urls')),
