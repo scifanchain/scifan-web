1bec56d5e5ac3b947b6a5c99d75a391164482da7
oxb <oxb@zhijian.run>
2021-06-15 20:34:10 +0800

    complete stage CRUD

--- src/stories/serializers.py
+++ src/stories/serializers.py
@@ -15,19 +15,19 @@ class StageListSerializer(serializers.ModelSerializer):
             'version',
             'content',
             'owner',
+            'authors',
         ]
 
-    # def create(self, validated_data):
-    #     # validated_data.authors.set(request.user)
-    #     # authors_data = validated_data.pop('authors')
-    #     instance = Stage.objects.create(**validated_data)
-    #     user_id = User.objects.get(pk=2)
-    #     instance.authors.add(user_id.id,)
-
-    #     return instance
-
-    # user = User.objects.only('id').get(id=data['user_id'])
-    # obj = ModelA.objects.create(phone=data['phone'], user=user)
+class StagePostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Stage
+        fields = [
+            'id',
+            'title',
+            'created_time',
+            'maturity',
+            'content',
+        ]
 
 class StageDetailSerializer(serializers.ModelSerializer):
     class Meta:
--- src/stories/views.py
+++ src/stories/views.py
@@ -2,7 +2,7 @@ from django.http.response import Http404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
-from .serializers import StageListSerializer, StageDetailSerializer
+from .serializers import StageListSerializer, StageDetailSerializer, StagePostSerializer
 from .models import Stage
 from rest_framework import generics
 from .permissions import IsAdminUserOrReadOnly
@@ -11,6 +11,12 @@ from django.contrib.auth.models import User
 class StageList(generics.ListCreateAPIView):
     queryset = Stage.objects.all()
     serializer_class = StageListSerializer
+
+    def get_serializer_class(self):
+        if self.request.method == 'GET':
+            return StageListSerializer
+        return StagePostSerializer
+
 
     def perform_create(self, serializer):
         # 有待完善：需结合验证，保证用户已登录并拥有相应权限
